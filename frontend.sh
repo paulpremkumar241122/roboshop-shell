4663e4b368348ee28cd1f9857c5090f5d0c85167
Paul Prem Kumar <paul.prem5@gmail.com>
2023-12-12 04:23:45 +0530

    Roboshop Nginx Script. updated headings using echo -e COL ...

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,20 @@
+
+echo -e "\e[33m INSTALLING NGINX SERVER \e[0m"
 dnf install nginx -y
 
+echo -e "\e[33m REMOVING OLD APP CONTENT \e[0m"
 rm -rf /usr/share/nginx/html/*
 
+echo -e "\e[33m  DOWNLOADING FRONTEND CONTENT  \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
+echo -e "\e[33m EXTRACTING FRONTEND CONTENT \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
+
 # here we need to copy and update config file
+
+echo -e "\e[33m STARTING NGINX SERVER \e[0m"
 systemctl enable nginx
 systemctl restart nginx
