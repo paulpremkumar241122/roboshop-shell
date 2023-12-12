a27f6ef2e94615aed1c4af193d74930536715f74
Paul Prem Kumar <paul.prem5@gmail.com>
2023-12-13 01:09:26 +0530

    MongoDB File... added redirectors

--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,12 @@
 echo -e "\e[33m Copy MongoDB repo file \e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongo.repo
+cp mongodb.repo /etc/yum.repos.d/mongo.repo  &>>/tmp/roboshop.log
+
 echo -e "\e[33m Installing MongoDB Server \e[0m"
-dnf install mongodb-org -y
+dnf install mongodb-org -y  &>>/tmp/roboshop.log
+
 echo -e "\e[32m Modify the config file \e[0m"
 # modify the config file
+
 echo -e "\e[33m start Mongodb Server \e[0m"
 systemctl enable mongod
 systemctl restart mongod
