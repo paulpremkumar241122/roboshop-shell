6bd77233a9cab897ab5156765aff166d65806a34
Paul Prem Kumar <paul.prem5@gmail.com>
2023-12-13 02:50:15 +0530

    updated mongoDB listen address using  sed command sed -i -e

--- mongodb.sh
+++ mongodb.sh
@@ -4,8 +4,8 @@ cp mongodb.repo /etc/yum.repos.d/mongo.repo  &>>/tmp/roboshop.log
 echo -e "\e[33m Installing MongoDB Server \e[0m"
 dnf install mongodb-org -y  &>>/tmp/roboshop.log
 
-echo -e "\e[32m Modify the config file \e[0m"
-# modify the config file
+echo -e "\e[33m Updating MongoDB Listen address \e[0m"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 echo -e "\e[33m start Mongodb Server \e[0m"
 systemctl enable mongod
