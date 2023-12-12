7796be49827215829756e4ed744e38b7b0e10ec2
Paul Prem Kumar <paul.prem5@gmail.com>
2023-12-13 00:47:31 +0530

    added redirectors... &>>

--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,19 @@
 
 echo -e "\e[33m INSTALLING NGINX SERVER \e[0m"
-dnf install nginx -y  >/tmp/roboshop.log
+dnf install nginx -y  &>>/tmp/roboshop.log
 
 echo -e "\e[33m REMOVING OLD APP CONTENT \e[0m"
-rm -rf /usr/share/nginx/html/*  >/tmp/roboshop.log
+rm -rf /usr/share/nginx/html/*  &>>/tmp/roboshop.log
+
 
 echo -e "\e[33m  DOWNLOADING FRONTEND CONTENT  \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  >/tmp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>/tmp/roboshop.log
+
 
 echo -e "\e[33m EXTRACTING FRONTEND CONTENT \e[0m"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip  >/tmp/roboshop.log
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
+
 
 
 # here we need to copy and update config file
