fa66ccd924d4dcab445e08e87c9de1b04d42b8f2
Paul Prem Kumar <paul.prem5@gmail.com>
2023-12-13 03:16:26 +0530

    Updated Frontend Configuration.

--- frontend.sh
+++ frontend.sh
@@ -5,18 +5,15 @@ dnf install nginx -y  &>>/tmp/roboshop.log
 echo -e "\e[33m REMOVING OLD APP CONTENT \e[0m"
 rm -rf /usr/share/nginx/html/*  &>>/tmp/roboshop.log
 
-
 echo -e "\e[33m  DOWNLOADING FRONTEND CONTENT  \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>/tmp/roboshop.log
-
 
 echo -e "\e[33m EXTRACTING FRONTEND CONTENT \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
-
-
-# here we need to copy and update config file
+echo -e "\e[33m UPDATE FRONTEND CONFIGURATION \e[0m"
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 echo -e "\e[33m STARTING NGINX SERVER \e[0m"
 systemctl enable nginx
