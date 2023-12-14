a5449cb96f652b6bcfdbd2a147b61cf976a52abd
Paul Prem Kumar <paul.prem5@gmail.com>
2023-12-15 02:37:44 +0530

    Re-Write

--- catalogue.sh
+++ catalogue.sh
@@ -1,42 +1,41 @@
-echo -e "\e[33m Configuring NodeJS Repos \e[0m"
-dnf module disable nodejs -y &>>/tmp/roboshop.log
-dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
+echo -e "\e[33m Enable NodeJS 18v \e[0m"
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
 
 echo -e "\e[33m Installing NodeJS \e[0m"
-dnf install nodejs -y &>>/tmp/roboshop.log
+dnf install nodejs -y
 
-echo -e "\e[33m Add Application User \e[0m"
-useradd roboshop &>>/tmp/roboshop.log
+echo -e "\e[33m Adding Application User \e[0m"
+useradd roboshop
 
-echo -e "\e[33m Create Application Directory \e[0m"
-rm-rf /app &>>/tmp/roboshop.log
-mkdir /app &>>/tmp/roboshop.log
+echo -e "\e[33m Creating Application Directory \e[0m"
+rm -rf
+mkdir /app
 
 echo -e "\e[33m Downloading Application Content \e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
-cd /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 
 echo -e "\e[33m Extracting Application Content \e[0m"
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
 cd /app
+unzip /tmp/catalogue.zip
 
-echo -e "\e[33m Install NodeJS Dependencies \e[0m"
-npm install &>>/tmp/roboshop.log
+echo -e "\e[33m Installing Application Dependencies \e[0m"
+cd /app
+npm install
 
 echo -e "\e[33m Setup SystemD Service \e[0m"
-cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
 
-echo -e "\e[33m Start Catalogue Service \e[0m"
+echo -e "\e[33m Starting Catalogue Service \e[0m"
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl restart catalogue
 
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl restart catalogue &>>/tmp/roboshop.log
+echo -e "\e[33m Copying Mongodb Repo \e[0m"
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo
 
-echo -e "\e[33m Copy MongoDB Repo file \e[0m"
-cp /home/centos/roboshop-shell/mongodb.repo /ete/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+echo -e "\e[33m Installing Mongodb \e[0m"
+dnf install mongodb-org-shell -y
 
-echo -e "\e[33m Install MongoDB Client \e[0m"
-dnf install mongodb-org-shell -y  &>>/tmp/roboshop.log
-
-echo -e "\e[33m Load Schema \e[0m"
-mongo --host mongodb-dev.vagdevi.store </app/schema/catalogue.js  &>>/tmp/roboshop.log
+echo -e "\e[33m Loading Schema \e[0m"
+mongo --host mongodb-dev.vagdevi.store </app/schema/catalogue.js
