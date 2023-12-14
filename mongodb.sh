602cc9a3bfca6aa89abd0540253be8599083d03f
Paul Prem Kumar <paul.prem5@gmail.com>
2023-12-14 16:10:04 +0530

    Again changed the path of conf file

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 echo -e "\e[33m Copy MongoDB repo file \e[0m"
-cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo  &>>/tmp/roboshop.log
+cp mongodb.repo /etc/yum.repos.d/mongo.repo  &>>/tmp/roboshop.log
 
 echo -e "\e[33m Installing MongoDB Server \e[0m"
 dnf install mongodb-org -y  &>>/tmp/roboshop.log
