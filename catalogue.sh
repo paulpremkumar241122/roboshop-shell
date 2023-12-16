5efb19fd29636d4cdd9e0eddc8bee67627cef05c
Paul Prem Kumar <paul.prem5@gmail.com>
2023-12-17 00:37:37 +0530

    Variables... component added

--- catalogue.sh
+++ catalogue.sh
@@ -1,3 +1,4 @@
+component=catalogue
 
 echo -e "\e[33m Enable NodeJs 18v \e[0m"
 dnf module disable nodejs -y  &>>/tmp/roboshop.log
@@ -10,26 +11,27 @@ echo -e "\e[33m Add User \e[0m"
 useradd roboshop  &>>/tmp/roboshop.log
 
 echo -e "\e[33m Creating Application Directory \e[0m"
+rm -rf  &>>/tmp/roboshop.log
 mkdir /app  &>>/tmp/roboshop.log
 
 echo -e "\e[33m Downloading Application Content \e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip  &>>/tmp/roboshop.log
+curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip  &>>/tmp/roboshop.log
 
 echo -e "\e[33m Extracting Application Content \e[0m"
 cd /app  &>>/tmp/roboshop.log
-unzip /tmp/catalogue.zip  &>>/tmp/roboshop.log
+unzip /tmp/$component.zip  &>>/tmp/roboshop.log
 
 echo -e "\e[33m Installing Dependencies \e[0m"
 cd /app  &>>/tmp/roboshop.log
 npm install  &>>/tmp/roboshop.log
 
 echo -e "\e[33m Setup Service File \e[0m"
-cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service  &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service  &>>/tmp/roboshop.log
 
-echo -e "\e[33m Starting Catalogue Service \e[0m"
+echo -e "\e[33m Starting $component Service \e[0m"
 systemctl daemon-reload  &>>/tmp/roboshop.log
-systemctl enable catalogue  &>>/tmp/roboshop.log
-systemctl restart catalogue  &>>/tmp/roboshop.log
+systemctl enable $component  &>>/tmp/roboshop.log
+systemctl restart $component  &>>/tmp/roboshop.log
 
 echo -e "\e[33m Copy Mongodb Repo \e[0m"
 cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo  &>>/tmp/roboshop.log
@@ -38,4 +40,4 @@ echo -e "\e[33m Installing Mongodb \e[0m"
 dnf install mongodb-org-shell -y  &>>/tmp/roboshop.log
 
 echo -e "\e[33m Loading Schema \e[0m"
-mongo --host mongodb-dev.vagdevi.store </app/schema/catalogue.js  &>>/tmp/roboshop.log
+mongo --host mongodb-dev.vagdevi.store </app/schema/$component.js  &>>/tmp/roboshop.log
