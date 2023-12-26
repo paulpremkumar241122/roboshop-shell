9b35b5628a6fabb87203fecc46d25dffec07f468
Paul Prem Kumar <paul.prem5@gmail.com>
2023-12-26 22:19:38 +0530

    final changes ...

--- dispatch.sh
+++ dispatch.sh
@@ -3,25 +3,10 @@ component=dispatch
 
 echo -e "${colour} Installing Golang ${nocolour}"
 dnf install golang -y  &>>${log_file}
+
 stat_check $?
 
-echo -e "${colour} Adding Application User ${nocolour}"
-useradd roboshop  &>>${log_file}
-stat_check $?
-
-echo -e "${colour} Creating Application Directory ${nocolour}"
-rm -rf  &>>${log_file}
-mkdir /app  &>>${log_file}
-stat_check $?
-
-echo -e "${colour} Downloading Application Content ${nocolour}"
-curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip  &>>${log_file}
-stat_check $?
-
-echo -e "${colour} Extracting Application Content ${nocolour}"
-cd /app  &>>${log_file}
-unzip /tmp/${component}.zip  &>>${log_file}
-stat_check $?
+app_pre_setup
 
 echo -e "${colour} Downloading the Dependencies ${nocolour}"
 cd /app  &>>${log_file}
@@ -30,11 +15,6 @@ go get  &>>${log_file}
 go build  &>>${log_file}
 stat_check $?
 
-echo -e "${colour} Copying the Service File ${nocolour}"
-cp /home/centos/roboshop-shell/${component}.service /etc/systemd/system/${component}.service  &>>${log_file}
-stat_check $?
+systemd_setup
 
-echo -e "${colour} Starting ${component} Service ${nocolour}"
-systemctl daemon-reload  &>>${log_file}
-systemctl enable ${component}  &>>${log_file}
 systemctl restart ${component} ; tail -f /var/log/messages
